730d523b897bc24e4f3943bbc72afdf3f39a673a
vituccibruno <vituccibruno@gmail.com>
2017-10-16 14:02:03 -0300

    actualizado

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=cpe_db',
-    'username' => 'cpewebuser',
+    'username' => 'cpedba',
     'password' => 'unaj1234',
     'charset' => 'utf8',
     'schemaMap' => [
