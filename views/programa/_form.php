8b416e69cebd8682f101fc5b3c003fdedb34777e
MatiasM <matiasmendoza1994@gmail.com>
2017-11-01 20:09:57 -0300

    Se repararon el modelo y las vistas programa

--- views/programa/_form.php
+++ views/programa/_form.php
@@ -2,7 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Programa */
@@ -13,27 +12,14 @@ use yii\helpers\ArrayHelper;
 
     <?php $form = ActiveForm::begin(); ?>
 
-<!--
     <?= $form->field($model, 'planmateria_id')->textInput() ?>
--->
-	<?= $form->field($model, "planmateria_id")
-				    ->dropDownList(
-            ArrayHelper::map($subModelPlanmateria->getAllPlanesmateria(), 'planmateria_id', 'planmateria_id'))
-    		?>
-<!--
+
     <?= $form->field($model, 'ano_id')->textInput() ?>
--->
-    <?= $form->field($model, "ano_id")
-				    ->dropDownList(
-            ArrayHelper::map($subModelAno->getAllAnos(), 'ano_id', 'ano'))
-    		?>
 
     <?= $form->field($model, 'fecha')->textInput() ?>
 
-    <?= $form->field($model, 'descripcion')->textInput(['maxlength' => true]) ?>
-
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
