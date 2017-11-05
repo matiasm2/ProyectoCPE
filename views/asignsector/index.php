353f1c43e7a4652cb4fc963cb66ba1fe1f8b71a8
DanielRosatto <danielrosatto@gmail.com>
2017-11-05 12:02:18 +0000

    Se deshabilita el search de asignsector/index porque no funciona y se actualizan permisos DELETE a usuario cpewebuser

--- views/asignsector/index.php
+++ views/asignsector/index.php
@@ -24,7 +24,7 @@ $this->title = 'Asignsectors';
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        //~ 'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
